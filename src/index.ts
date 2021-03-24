79a78eb397802e47863cf8e5a72d925709eca9c8
vinicius-oliveira7787 <vinicius.oliveira7787@gmail.com>
2021-03-24 16:25:39 -0300

    começando com testes unitários na ficha1/Vinicius

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 //* função firstExerciseA para este arquivo
 //! Estou importando *(Todos os arquivos) as(como) com o nome de Exercises do arquivo app.ts
 import * as Exercises from "./app"
+import { Exercises1, Exercises2, Exercises3, Exam } from './vinicius'
 
 // const namesArray: Array<string> = ['Huann','vinicius','lucas','Rua','Matheus']
 // const agesArray: Array<number> = [18,16,0,23,15,45]
---
+++ src/vinicius/__tests__/Exercises1.test.ts
@@ -0,0 +1,11 @@
+import * as Exercises from '../Exercises1'
+
+describe('testing first exercise', () => {
+  test('exercise A', () => {
+    expect(Exercises.Exercise1A()).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+  
+  test('exercise B', () => {
+    expect(Exercises.Exercise1B()).toStrictEqual([10,9,8,7,6,5,4,3,2,1]);
+  });
+});
